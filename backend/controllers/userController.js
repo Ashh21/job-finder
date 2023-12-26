812b99ba7517b02348108311cc2f1e5ac3207883
Ashh21 <106957781+Ashh21@users.noreply.github.com>
2023-12-26 16:04:28 +0530

    css update

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -23,7 +23,7 @@ const create = async (req, res, next) => {
             name, email, mobile, password: encryptPassword
         })
 
-        const jwttoken = jwt.sign({ email, _id: user._id }, process.env.JWT_SECRET_KEY, { expiresIn: '24h' })
+        const jwttoken = jwt.sign({ email, _id: user._id }, process.env.JWT_SECRET_KEY, { expiresIn: '1h' })
         res.status(200).json({
             jwttoken,
             reacruiterName: user.name,
@@ -63,7 +63,7 @@ const login = async (req, res, next) => {
 
         if (isPasswordMatched) {
             const token = { _id: user._id, email: user.email }
-            const jwttoken = jwt.sign(token, process.env.JWT_SECRET_KEY, { expiresIn: '24h' })
+            const jwttoken = jwt.sign(token, process.env.JWT_SECRET_KEY, { expiresIn: '1h' })
             return res.status(200).json({
                 message: "Login successful",
                 jwttoken,
